61ca8b7b77b49b5dc41255914ea7df7ccfdc8546
Thomas Soulard <windowsvista7helper@gmail.com>
2018-12-25 15:43:37 +0000

    fixes so now the app runs!

--- ClientApp/src/components/EveAuth/Login.js
+++ ClientApp/src/components/EveAuth/Login.js
@@ -3,14 +3,10 @@ import React, { Component } from 'react'
 export class Login extends Component {
     displayName = Login.name
 
-    // constructor(props){
-    //     super(props);
-    // }
-
     createLoginUrl() {
-        var x = "https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=http://localhost:5002/callback&client_id=253a2a78c5434b3dae3c9d9002a3b537&scope=esi-characters.read_standings.v1";
+        var loginUrl = "https://login.eveonline.com/oauth/authorize?response_type=code&redirect_uri=http://localhost:5002/callback&client_id=253a2a78c5434b3dae3c9d9002a3b537&scope=esi-characters.read_standings.v1";
 
-        return x;
+        return loginUrl;
     }
 
     render(){
